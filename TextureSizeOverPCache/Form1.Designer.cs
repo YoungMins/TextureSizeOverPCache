648da887accfca066d0c906ac5df13517a9cc947
YoungMins <nanyoungmin@gmail.com>
2019-08-22 23:05:10 +0900

    제작 완료!

---
+++ TextureSizeOverPCache/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TextureSizeOverPCache
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(38, 37);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(334, 36);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Convert";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 112);
+            this.Controls.Add(this.btnRun);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Texture To PointCache";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ TextureSizeOverPCache/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextureSizeOverPCache
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opendlg = new OpenFileDialog();
+            opendlg.Filter = "PNG File|*.png|Bitmap File|*.bmp|JPEG File|*.jpg|TIFF File|*.tif";
+
+            if ( opendlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Image img = Image.FromFile(opendlg.FileName);
+                    Size size = img.Size;
+
+                    string pCacheStr = pCacheHeader(size.Width * size.Height);
+
+                    using (Bitmap bitmap = new Bitmap(img))
+                    {
+                        for( int j=0; j< size.Height; j++)
+                        {
+                            for( int i=0; i<size.Width; i++)
+                            {
+                                float x = Remap(i, 0, size.Width, -0.5f, 0.5f);
+                                float y = Remap(j, 0, size.Height, -0.5f, 0.5f);
+                                float z = 0;
+
+                                Color col = bitmap.GetPixel(i, j);
+                                float r = Remap(col.R, 0, 255, 0, 1);
+                                float g = Remap(col.G, 0, 255, 0, 1);
+                                float b = Remap(col.B, 0, 255, 0, 1);
+                                float a = Remap(col.A, 0, 255, 0, 1);
+                                pCacheStr += 
+                                    (x.ToString() + " " +
+                                     y.ToString() + " " +
+                                     z.ToString() + " " +
+                                     r.ToString() + " " +
+                                     g.ToString() + " " +
+                                     b.ToString() + " " +
+                                     a.ToString() + "\n");
+                            }
+                        }                        
+                    }
+                    SaveFileDialog saveDlg = new SaveFileDialog();
+                    saveDlg.Filter = "point cache file | *.pCache";
+                    if (saveDlg.ShowDialog() == DialogResult.OK)
+                    {
+                        Encoding utf8WithoutBom = new UTF8Encoding(false);
+                        File.WriteAllText(saveDlg.FileName, pCacheStr, utf8WithoutBom);
+                        MessageBox.Show("Success");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private string pCacheHeader(int count)
+        {
+            string str = string.Empty;
+            str = "pcache\n" +
+            "format ascii 1.0\n" +
+            "comment Make by YOUNGMIN KIM\n" +
+            "elements " + count.ToString() + "\n" +
+            "property float position.x\n" +
+            "property float position.y\n" +
+            "property float position.z\n" +
+            "property float color.r\n" +
+            "property float color.g\n" +
+            "property float color.b\n" +
+            "property float color.a\n" +
+            "end_header\n";
+
+            return str;
+        }
+
+        public float Remap(float value, float from1, float to1, float from2, float to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+    }
+}
